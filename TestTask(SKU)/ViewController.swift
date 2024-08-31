be8fa7926f9cd1fbe26d17958ebc4f502721c394
AlexNov12 <nov.alex96@gmail.com>
2024-08-31 07:59:31 +0300

    Initial Commit

---
+++ TestTask(SKU)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask(SKU)
+//
+//  Created by Александр Новиков on 31.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
